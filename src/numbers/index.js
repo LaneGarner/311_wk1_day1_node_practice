ff34b7a1c0825f9cbfc6c89db43eba34cbda2224
Lane Garner <b.lane.garner@gmail.com>
2020-11-30 11:27:33 -0600

    passing tests

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,30 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if(num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((acc, cur) => acc + cur)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let x in arr) {
+    for (let y in arr) {
+      if (x + y === sum) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
 }
+
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,21 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split([delim])
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  myArr = []
+  for (var i = 0; i < str.length; i += 2) {
+    // return str.match(/.{2}/g)
+    myArr.push(str.slice(i, i+2))
+  }
+  return myArr
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('')
 }
 
 module.exports = {
